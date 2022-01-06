41ae6873e5d1c7cce224ae7510d5f36f3d3b5681
Augusto <augustoacha177@gmail.com>
2022-01-06 17:47:14 -0300

    "Player 1's codelines seting inside function"

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,22 +1,40 @@
 import os;
 def main():
     s=True
+    counter_time = 0
     #array with nine elements/creo una lista de nueve elementos
     os.system('cls')
     positions=[1,2,3,4,5,6,7,8,9]
     player1 = "X"
     player2 = "O"
+    
+    print("Tic-Tac-Toe Game")
     interface(positions)
-    position_picked_player1 = int(input("Player 1 Choice the position to draw your Simbol :"))
-    print("Positions should be betwen 1 and 9")
-    if (position_picked_player1 > 0 and position_picked_player1 < 10):
-            positions[position_picked_player1-1]="X"
-            interface(positions)
+    print()
+    print()
+    while counter_time < 10:
+        position_picked_player1 = int(input("Player 1 Choice the position to draw your Simbol :"))
+        print("Positions should be betwen 1 and 9")
+        #This function call to Player_one to pick position
+        player_one(positions, position_picked_player1)
+        player_two()
+        
+        
             
     print()
     print()
-    
+def player_one(positions, position_picked_ply1):
+    if (position_picked_ply1 > 0 and position_picked_ply1 < 10):
+                positions[position_picked_ply1-1]="X"
+                interface(positions)
+                
+                    
+def player_two():
+    pass
+
 def interface(position):
+    print()
+    print()
     for i in position:
         #set separator "|"
         print(i, end =" | ")
