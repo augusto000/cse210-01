7613e1bec9b6d204c947d879c1a80802064f5833
Augusto <augustoacha177@gmail.com>
2022-01-06 16:19:59 -0300

    "First Player start drawing the X"

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,21 +1,33 @@
 import os;
 def main():
-    #creo una lista de nueve elementos
-    elementos=[1,2,3,4,5,6,7,8,9]
+    s=True
+    #array with nine elements/creo una lista de nueve elementos
     os.system('cls')
-    interfaz(elementos)
+    positions=[1,2,3,4,5,6,7,8,9]
+    player1 = "X"
+    player2 = "O"
+    interface(positions)
+    position_picked_player1 = int(input("Player 1 Choice the position to draw your Simbol :"))
+    print("Positions should be betwen 1 and 9")
+    if (position_picked_player1 > 0 and position_picked_player1 < 10):
+            positions[position_picked_player1-1]="X"
+            interface(positions)
+            
     print()
     print()
     
-def interfaz(elements):
-    for i in elements:
-        #crear separador "|"
+def interface(position):
+    for i in position:
+        #set separator "|"
         print(i, end =" | ")
         if i == 3:
+            #print next line down with that simbols
             print("\n - + - + -")
         elif i == 6:
+            #next line down
             print("\n")
         elif i==9:
+            #print next line down with that simbols
             print("\n - + - + -")
 
 if __name__== "__main__":
