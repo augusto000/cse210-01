7bfd95e2dfbac6a690a15292c27063e9c1667626
Augusto <augustoacha177@gmail.com>
2022-01-08 11:13:19 -0300

    "It start using a matrix for the board"

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,54 +1,57 @@
 import os;
-import keyboard;
-import pyautogui as pa;
-import random;
 def main():
     s=True
     counter_time = 0
-    #array with nine elements/creo una lista de nueve elementos
+   
     os.system('cls')
-    positions=[1,2,3,4,5,6,7,8,9]
-    player1 = "X"
-    player2 = "O"
+      
     
-    print("Tic-Tac-Toe Game")
-    interface(positions)
-    print()
-    print()
-    while counter_time < 10:
-        position_picked_player1 = int(input("Player 1 Choice the position to draw your Simbol :"))
-        print("Positions should be betwen 1 and 9")
-        os.system('crtl+ñ')
-        #This function call to Player_one to pick position
-        player_one(positions, position_picked_player1)
-        player_two()
-        
-        
-            
-    print()
-    print()
-def player_one(positions, position_picked_ply1):
-    if (position_picked_ply1 > 0 and position_picked_ply1 < 10):
-                positions[position_picked_ply1-1]="X"
-                interface(positions)
+       #interface() devuelve el tablero para empeza a jugar
+    interfa= interface()
+    #Luego de imprimir el tablero de juego, pido que ingrese la posicion
+    #a escribir el simbolo x
+    posicion_escogida_x = int(input("Jugador 1 Ingrese Posicion escogida "))
+    inte = marco_x(interfa, posicion_escogida_x)    
+    posicion_escogida_o = int(input("Jugador 2 Ingrese Posicion escogida :"))
+    marco_o(inte, posicion_escogida_o)
+    
+def marco_o(inte, posicion_escogida_o):
+    for i in range(3):
+        for j in range(3):
+            if inte[i][j] == posicion_escogida_o:
+                inte[i][j] = "o"
+                print(inte[i][j], end = " ")
+            else:
+                print(inte[i][j], end = " ")
+        print(sep = "\n")
+    return inte    
                 
-                    
-def player_two():
-    pass
-
-def interface(position):
-    print()
-    print()
-    for i in position:
-        #set separator "|"
-        print(i, end =" | ")
-        if i == 3:
-            #print next line down with that simbols
-            print("\n - + - + -")
-        elif i == 6:
-            #next line down
-            print("\n- + - + -")
+def marco_x(interfa, posicion_escogida_x):
+    #inter is the positions picked at the interface
+    for i in range(3):
+        for j in range(3):
+            if interfa[i][j] == posicion_escogida_x:
+                interfa[i][j] = "x"
+                print(interfa[i][j], end = " ")
+            else:
+                print(interfa[i][j], end = " ")
+                        
+        print(sep = "\n")
+    return interfa        
         
+def interface():
+     #matriz with nine elements/creo una matriz de nueve elementos
+    positions = [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]        
+                ]        
+    for i in positions:
+        for elemento in i:
+            print(elemento, end=" ")
+        print(sep="\n") 
+    return positions       
+
 
 if __name__== "__main__":
     main()
