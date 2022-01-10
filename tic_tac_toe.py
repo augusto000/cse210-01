baa09bedccad3cf046e8fd7dbe35b818592e8e8a
Augusto <augustoacha177@gmail.com>
2022-01-10 10:08:18 -0300

    "already coded both user input data from theirs own functions"

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,23 +1,31 @@
 import os;
 def main():
-   
     os.system('cls')
-      
-    #interface()draw The Board / devuelve el tablero para empeza a jugar
-    interfa= interface()
+    s_x = True
+    s_o = True
+           
+    #os.system('cls')
+    #interface()draw The Board / devuelve el tablero para empeza a jugar  
+    interfa=interface()
     
+    x = callInput_Player1_x()
+    
+    if (x >= 1 and x <= 9): 
+        #call to spot the position choosen of Player 1
+        inte_x = marco_x(interfa, x)
+        o = callInput_Player2_o()
+        inte_o = marco_o (inte_x, o) 
+        print()
+            
+def callInput_Player2_o():
+    o = int(input("Player 2 Input the choosen position :"))
+    return o    
+
+def callInput_Player1_x():
     #Player 1 for the choosen position 
-    posicion_escogida_x = int(input("Player 1 Input the choosen position :"))
-    os.system('cls')
-    #call to spot the position choosen of Player 1
-    inte = marco_x(interfa, posicion_escogida_x)    
-    
-    #Player 2 input your choosen position
-    posicion_escogida_o = int(input("Player 2 Input the choosen position :"))
-    
-    #call to spot the position choosen of Player 2
-    marco_o(inte, posicion_escogida_o)
-    
+    x = int(input("Player 1 Input the choosen position :"))
+    return x
+
 def marco_o(inte, posicion_escogida_o):
     #this function draw the board with the o position choosen from Player 2
     for i in range(3):
@@ -29,19 +37,20 @@ def marco_o(inte, posicion_escogida_o):
                 print(inte[i][j], end = " ")
         print(sep = "\n")
     return inte    
-                
-def marco_x(interfa, posicion_escogida_x):
+
+def marco_x(interf, posicion_escogida_x):
     #this function draw the board with the x position choosen from Player 1
     for i in range(3):
         for j in range(3):
-            if interfa[i][j] == posicion_escogida_x:
-                interfa[i][j] = "x"
-                print(interfa[i][j], end = " ")
+            if interf[i][j] == posicion_escogida_x:
+                interf[i][j] = "x"
+                print(interf[i][j], end = " ")
             else:
-                print(interfa[i][j], end = " ")
+                print(interf[i][j], end = " ")
                         
         print(sep = "\n")
-    return interfa        
+    return interf        
+
         
 def interface():
      #Draw the matriz with nine elements/dibujo una matriz de nueve elementos
